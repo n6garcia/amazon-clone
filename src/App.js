09d13352536ca88eaeb2cb95ead0231d3a294712
Noel <n6garcia@ucsd.edu>
2022-09-09 03:51:18 -0700

    added home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
